cf6c7cae37bab7a42479b305e81205c7a30ba35f
John Hartert <j.blainehartert@gmail.com>
2017-08-30 10:12:26 -0400

    Adds comments organizing everything.

--- WorldClock.js
+++ WorldClock.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
+// This class extends the component react.
 class WorldClock extends Component {
     constructor(props) {
         super(props);
+        // Object
         this.state = {
             currentTime: (new Date()),
             clockFace: 'normal',
@@ -11,13 +13,18 @@ class WorldClock extends Component {
         }
     }
     _updateTime = (timeDifference) => {
+        // Inherits currentTime from this.state. 
         const newTime = new Date(this.state.currentTime.getTime)
+        // Makes a new object that gives currentTime a new time (newTime).
         this.setState({
             currentTime: newTime
         });
     }
+    // isDigital and newColor are new arguments updating the digital and color objects 
+    //from above. Inherits from the original class above.
     _updateLookAndFeel = (isDigital, newColor) => {
         this.setState({
+            // Assigns digital and color new values.
             digital: isDigital,
             color: newColor
         });
